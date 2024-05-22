7aecc170644b573f57d87233bcb12ba788024810
Jacques Faulkner <hex@hexdev.xyz>
2024-05-22 04:46:47 +0100

    split /patients in order to be able to parse data

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const log = require('./utils/log.js');
 
+require('dotenv').config();
+
+const patients = require('./classes/patients.js');
+
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/cdn/*', (req, res) => {
     log.info(`${req.ip} GET ${req.url}`);
     res.sendFile(__dirname + "/public/" + req.url.split('/').slice(2).join('/'));
 });
+
+app.get('/patients', async (req, res) => {
+    log.info(`${req.ip} GET ${req.url}`);
+    const patientList = await patients.getAllPatients();
+    res.render('patients/index.ejs', {query: req.query, patientList: patientList});
+});
+
+app.get('/patient', async (req,res) => {
+    log.info(`${req.ip} GET ${req.url}`)
+    if(!req.query.id) {
+        req.redirect('/patients')
+    } else {
+        const id = req.query.id;
+        const patient = await patients.getPatient(id);
+        res.render('patients/patient.ejs', {patient: patient})
+    }
+});
+
+
 
 app.get('/*', (req, res) => {
     log.info(`${req.ip} GET ${req.url}`);
@@ -19,7 +45,7 @@ app.get('/*', (req, res) => {
             res.sendStatus(404);
             break;
         default:
-            let theURL = req.url.split('/').slice().join('/');
+            let theURL = req.url.split('/').slice(1).join('/');
             res.render(theURL + '.ejs', {query: req.query});
     }
 });
